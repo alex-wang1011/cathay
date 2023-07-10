b74f90aa629141cf680aac8fe37eb9a55a9824b8
alex-wang1011 <139105700+alex-wang1011@users.noreply.github.com>
2023-07-10 18:15:51 +0800

    Add files via upload
    更新程式碼

--- 國泰/國泰_程式邏輯三.py
+++ 國泰/國泰_程式邏輯三.py
@@ -1,19 +1,18 @@
 # 程式邏輯題目三
 def find_last_person(n):
-    # 創建同事列表，初始為順序排號
+    if n == 0:
+        return 0
+
     colleagues = list(range(1, n + 1))
 
-    # 當同事列表中還有超過一個人時，執行報數並移除第三個人，直到只剩下最後一個人
     index = 0
     while len(colleagues) > 1:
-        index = (index + 2) % len(colleagues)  # 報數到3的位置
+        index = (index + 2) % len(colleagues)
         colleagues.pop(index)
 
     return colleagues[0]
 
-# 輸入同事人數
 n = int(input("請輸入同事的人數: "))
 
-# 計算最後留下的同事的順位
 last_person = find_last_person(n)
 print("最後留下的同事的順位是:", last_person)
